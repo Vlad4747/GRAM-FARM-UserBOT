d89188bd702be2d52280e2f764ca480ab81e33b5
SystemLord <101967762+Vlad4747@users.noreply.github.com>
2024-12-05 21:59:25 +0500

    Add files via upload

--- modules/farm1.py
+++ modules/farm1.py
@@ -10,54 +10,57 @@ import random
 
 phone = input(cyan+"Напишите номер телефона (+71234567890): ")
 print(clear)
+s2 = True
+def func():
+    bot = Client(name=phone,
+                 api_id=24976468,
+                 api_hash='6e498035dcf4d32e270ff3f90cf61c58',
+                 phone_number=phone)
+
+
+    @bot.on_message(filters.text)
+    async def echo_handler(client: Client, message: Message):
+        global s2
+        try:
+            split = message.text.split()
+            if s2:
+                await bot.send_message(chat_id=6974533139, text='👨‍💻 Заработать')
+                print(lgreen + "Отправлено: Заработать" + clear)
+                s2 = False
+            if message.from_user.id == 6974533139:
+
+                if "💰" == split[0] and "Вы" == split[1] and "можете" == split[2]:
+                    await message.click(0, 2)
+                    time.sleep(0.01)
+                    await bot.send_message(chat_id=6974533139, text='.')
+                    print(lgreen + "Отправлено: ." + clear)
+                    time.sleep(1)
+                    print(lgreen+f"Нажатие на кнопку {clear}[0, 2]")
+                elif "Чтобы" == split[0] and "получать" == split[1] and "грамм," == split[2]:
+                    await message.click(0, 0)
+                    print(lgreen+f"Нажатие на кнопку {clear}[0, 0]")
+                elif "💰" == split[0] and "Вам" == split[1] and "начислено" == split[2]:
+                    await message.click(0, 0)
+                    print(lgreen+f"Нажатие на кнопку {clear}[0, 0]")
+                elif "❌ Больше нет постов для просмотра." == message.text or "❌ Нету доступных заданий" == message.text:
+                    print(cyan + "Задания на просмор постов закончились")
+                    print("Проверка новых заданий будет через 3 минуту")
+                    print(clear)
+                    print(lgreen + "Перезапуск..." + clear)
+                    return None
+        except:
+            print(red + "Ошибка при обработке сообщения" + clear)
+        time.sleep(1)
+
+
+
+    bot.run()
 
 while True:
     try:
-        bot = Client(name=phone,
-                     api_id=24976468,
-                     api_hash='6e498035dcf4d32e270ff3f90cf61c58',
-                     phone_number=phone)
-        s1 = True
-        s2 = True
-        @bot.on_message(filters.text)
-        async def echo_handler(client: Client, message: Message):
-                global s1,s2
-                try:
-                    split = message.text.split()
-                    if s2:
-                        await bot.send_message(chat_id=6974533139, text='👨‍💻 Заработать')
-                        s2 = False
-                    if message.from_user.id == 6974533139:
-
-                        if "💰" == split[0] and "Вы" == split[1] and "можете" == split[2]:
-                            s1 = False
-                            await message.click(0,2)
-
-                            await bot.send_message(chat_id=6974533139, text='.')
-                            time.sleep(1)
-                        elif "Чтобы" == split[0] and "получать" == split[1] and "грамм," == split[2]:
-                            await message.click(0, 0)
-                        elif "💰" == split[0] and "Вам" == split[1] and "начислено" == split[2]:
-                            await message.click(0, 0)
-                        elif "❌ Больше нет постов для просмотра." == message.text or "❌ Нету доступных заданий" == message.text:
-                            print(cyan+"Задания на просмор постов закончились")
-                            print("Проверка новых заданий будет через 1 минуту")
-                            print(clear)
-                            time.sleep(60+random.random())
-                            await bot.send_message(chat_id=6974533139, text='👨‍💻 Заработать')
-                except:
-                    print(red+"Ошибка при обработке сообщения"+clear)
-                time.sleep(1)
-        async def main():
-            global s1
-            async with bot:
-                async for dialog in bot.get_dialogs():
-                    time.sleep(1)
-                    if s1:
-                        pass
-
-
-
-        bot.run()
+        print(lgreen+"Запуск..."+clear)
+        func()
+        time.sleep(180+random.random()*3)
+        print(lgreen+bold + "3 минута прошла" + clear)
     except:
         pass
--- modules/farm3.py
+++ modules/farm3.py
@@ -6,6 +6,7 @@ from pyrogram.types import Message
 
 from ui import *
 import time
+import sys
 
 phone = input(cyan+"Напишите номер телефона (+71234567890): ")
 print(clear)
@@ -34,24 +35,23 @@ while True:
 
                             await bot.send_message(chat_id=6974533139, text='.')
                             time.sleep(1)
-                        elif message.text == '''Нажмите на кнопки, чтобы выбрать задание
-
-⚠️ Запрещено отписываться ранее чем через 7 дней от каналов
-
-в противном случае ваша возможность выполнять задания будет заблокирована, а заработанные средства аннулированы.''':
-                            await message.click(0, 0)
+                        elif split[0]+split[1]+split[2] == '''Нажмитенакнопки,''':
+                            print(message.reply_to_message)
                             await bot.send_message(chat_id=6974533139, text='.')
                             time.sleep(1)
-                        elif "💰" == split[0] and "Вам" == split[1] and "начислено" == split[2]:
-                            await message.click(0, 0)
-                        elif "❌ Больше нет постов для просмотра." == message.text or "❌ Нету доступных заданий" == message.text:
-                            print(cyan+"Задания на просмор постов закончились")
-                            print("Проверка новых заданий будет через 1 минуту")
-                            print(clear)
-                            time.sleep(60)
-                            await bot.send_message(chat_id=6974533139, text='👨‍💻 Заработать')
+
+                        try:
+                            print(message.reply_markup["inline_keyboard"])
+                            print(message.reply_markup)
+                            print(message.text)
+                        except:
+                            pass
+
                 except:
-                    print(red+"Ошибка при обработке сообщения"+clear)
+                    print(red,"Error:",sys.exc_info(),clear)
+                    if sys.exc_info()[1] == "Request timed out":
+                        time.sleep(0.4)
+
                 time.sleep(1)
         async def main():
             global s1
